67484f5cd16c2cec1e3b8a5b874d27af27fa98a2
Vishwanth C R <vishwanthcr08@gmail.com>
2026-07-09 17:00:14 +0000

    [LeetCode Sync] Runtime - 2 ms (99.95%), Memory - 52.7 MB (29.58%)

---
+++ leetcode-solutions/0229-majority-element-ii/solution.java
@@ -0,0 +1,45 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        int n=nums.length;
+        int count1=0;
+        int count2=0;
+        int el1 = Integer.MIN_VALUE;
+        int el2= Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            if(count1==0 && el2 != nums[i]){
+                count1++;
+                el1=nums[i];
+            }
+            else if(count2==0 && el1 !=nums[i]){
+                count2++;
+                el2=nums[i];
+            }
+            else if(nums[i] == el1){
+                count1++;
+            }
+            else if(nums[i] == el2){
+                count2++;
+            }
+            else{
+                count1--;
+                count2--;
+            }
+        }
+
+        count1=0; count2=0;
+        for(int i =0;i<n;i++){
+            if(nums[i]==el1){
+                count1++;
+            }
+            if(nums[i]==el2){
+                count2++;
+            }
+        }
+
+        List<Integer> result= new ArrayList<>();
+        int val = n/3 +1;
+        if(count1>=val) result.add(el1);
+        if(count2>=val) result.add(el2);
+        return result;
+    }
+}
