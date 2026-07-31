33f7ac0c65e8f2205e6d10b15ff5b62de13ce17f
Vishwanth C R <vishwanthcr08@gmail.com>
2026-07-31 12:59:53 +0000

    [LeetCode Sync] Runtime - 11 ms (87.84%), Memory - 47.4 MB (19.38%)

---
+++ leetcode-solutions/0451-sort-characters-by-frequency/solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String frequencySort(String s) {
+        Map<Character, Integer> counts = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            counts.put(c, counts.getOrDefault(c, 0) + 1);
+        }
+
+        List<Character>[] buckets = new List[s.length() + 1];
+        for (char key : counts.keySet()) {
+            int freq = counts.get(key);
+            if (buckets[freq] == null) {
+                buckets[freq] = new ArrayList<>();
+            }
+            buckets[freq].add(key);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = buckets.length - 1; i > 0; i--) {
+            if (buckets[i] != null) {
+                for (char c : buckets[i]) {
+                    for (int j = 0; j < i; j++) {
+                        sb.append(c);
+                    }
+                }
+            }
+        }
+
+        return sb.toString();
+    }
+}
