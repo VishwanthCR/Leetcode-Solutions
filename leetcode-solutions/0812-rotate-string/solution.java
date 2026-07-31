fa6a6f299a846182dabdec0e9fba75d0aa96b714
Vishwanth C R <vishwanthcr08@gmail.com>
2026-07-31 10:19:03 +0000

    [LeetCode Sync] Runtime - 2 ms (64.61%), Memory - 43.5 MB (31.96%)

---
+++ leetcode-solutions/0812-rotate-string/solution.java
@@ -0,0 +1,55 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length()) return false;
+        if (s.isEmpty()) return true;
+
+        String text = s + s;
+        return kmpSearch(text, goal);
+    }
+
+    private boolean kmpSearch(String text, String pattern) {
+        int[] lps = computeLPSArray(pattern);
+        int i = 0;
+        int j = 0;
+
+        while (i < text.length()) {
+            if (pattern.charAt(j) == text.charAt(i)) {
+                i++;
+                j++;
+            }
+            if (j == pattern.length()) {
+                return true;
+            } else if (i < text.length() && pattern.charAt(j) != text.charAt(i)) {
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else {
+                    i++;
+                }
+            }
+        }
+        return false;
+    }
+
+    private int[] computeLPSArray(String pattern) {
+        int n = pattern.length();
+        int[] lps = new int[n];
+        int len = 0;
+        int i = 1;
+
+        while (i < n) {
+            if (pattern.charAt(i) == pattern.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return lps;
+    }
+}
