59ec9ce317b5b8d5b752cd8d926e15d4543210d4
Vishwanth C R <vishwanthcr08@gmail.com>
2026-08-01 17:25:53 +0000

    [LeetCode Sync] Runtime - 1 ms (100.00%), Memory - 43.4 MB (97.14%)

---
+++ leetcode-solutions/0008-string-to-integer-atoi/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int myAtoi(String s) {
+        int i=0;
+        while(i<s.length() && s.charAt(i) == ' ') i++;
+        int sign =1;
+        if(i<s.length() && (s.charAt(i) == '+' || s.charAt(i) == '-')){
+            sign = (s.charAt(i)== '-') ? -1 : 1;
+            i++;
+        }
+        return helper(s,i,0L,sign);
+    }
+    public int helper(String s,int i,long num,int sign){
+        while(i>=s.length() || !Character.isDigit(s.charAt(i))){
+            return (int)(sign*num);
+        }
+        num = num*10 + (s.charAt(i) - '0');
+        if(sign*num <= Integer.MIN_VALUE) return Integer.MIN_VALUE;
+        else if(sign*num >= Integer.MAX_VALUE) return Integer.MAX_VALUE;
+        return helper(s,i+1,num,sign);
+    }
+}
