59b3dd27b33924c1ac23b99cf041d366188c1166
Vishwanth C R <vishwanthcr08@gmail.com>
2026-08-13 14:10:12 +0000

    [LeetCode Sync] Runtime - 132 ms (29.39%), Memory - 53 MB (39.27%)

---
+++ leetcode-solutions/0282-expression-add-operators/solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<String> addOperators(String num, int target) {
+        List<String> result = new ArrayList<>();
+        dfs(num, target, 0, 0, 0, "", result);
+        return result;
+    }
+    public void dfs(String num, int target, int start, long current_value, long last_operand, String expression, List<String> result) {
+        if (start == num.length()) {
+            if (current_value == target) {
+                result.add(expression);
+            }
+            return;
+        }
+        for (int i = start; i < num.length(); i++) {
+            if (i > start && num.charAt(start) == '0') return;
+            String current_num = num.substring(start, i + 1);
+            long current_num_val = Long.parseLong(current_num);
+            if (start == 0) {
+                dfs(num, target, i + 1, current_num_val, current_num_val, current_num, result);
+            } else {
+                dfs(num, target, i + 1, current_value + current_num_val, current_num_val, expression + "+" + current_num, result);
+                dfs(num, target, i + 1, current_value - current_num_val, -current_num_val, expression + "-" + current_num, result);
+                dfs(num, target, i + 1, current_value - last_operand + last_operand * current_num_val, last_operand * current_num_val, expression + "*" + current_num, result);
+            }
+        }
+    }
+}
